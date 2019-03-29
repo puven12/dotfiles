253d705819523c6194ddd91f72141cb9fae75ee7
puven12 <puven12@gmail.com>
2019-03-29 01:11:16 +0100

    Add cmus

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,14 @@
+/home/puven/Music/8779879133209954145.mp3
+/home/puven/Music/Apple of My Eye-eQviza7F3oE.opus
+/home/puven/Music/Deep Dark & Hard Underground Techno Mix 2018-gbVITmbjXxk.webm
+/home/puven/Music/German Underground Techno 2018  _ Dark & Hard _ Fear & Loathing in Berlin-cjF-9In3hqU.webm
+/home/puven/Music/German Underground Techno 2018 _ Dark & Hard _ München Ostbahnhof-lJ_aqxOU6Kg.webm
+/home/puven/Music/HMLTD - Pictures of You (Official Video)-WeNMmQs7eFk.opus
+/home/puven/Music/HMLTD - Proxy Love (Official Video)-rMO9Rkfx1zc.opus
+/home/puven/Music/HMLTD - Proxy Love (Soft Cell Remix) [Audio]-7qSs916jXwo.opus
+/home/puven/Music/Mac Quayle - Emmy Nominated Score - Mr. Robot '1.0_3-fucksociety.mp3'-eiEiyTpK1Vc.mp4
+/home/puven/Music/Mac Quayle  - Emmy Nominated Score - Mr. Robot  '1.0_8-whatsyourask.m4p'-OQOeePhNj-I.mp4
+/home/puven/Music/Mac Quayle - Emmy Nominated Score - Mr. Robot '1.3_5-da3monsneverstop caf'-cPH53hOU4pA.mp4
+/home/puven/Music/Mac Quayle - Emmy Nominated Score - Mr. Robot '1.4_2-impenetrable sd2'-i61Sq7LEZ60.mp4
+/home/puven/Music/Mannequin-WsgqTiV_6qs.opus
+/home/puven/Music/Bird Set Free - Sia.mp3
